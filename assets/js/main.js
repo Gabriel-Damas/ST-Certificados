419ed8f3a1e8ef2d4c5546491784a3966d7e897b
Gabriel Damasceno <76968249+Gabriel-Damas@users.noreply.github.com>
2022-07-31 10:10:19 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
